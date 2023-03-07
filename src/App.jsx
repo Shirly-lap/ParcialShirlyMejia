3f70e7652f695aba5130553afd2886d8c0aa270a
Shirly-lap <mejiaacostashirlipaola@gmail.com>
2023-03-07 13:37:39 -0500

    Inicio parcial

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
-
 import './App.css'
-import Home from './components/Home'
+import Card from './components/Card'
+
 
 function App() {
+
   return (
     <>
-    <Home />
+    <h1>Carga de estudiantes</h1>
+      <form></form>
+
+    <Card/>
     </>
   )
 }
